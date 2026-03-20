b85894a1180d5ee71c44635a432f71286f17f085
Mohan Sai Singu <mohansaisingu23@gmail.com>
2026-03-19 22:07:44 -0700

    added qr code service

--- src/main/java/com/qrgate/core/service/QrCodeService.java
+++ src/main/java/com/qrgate/core/service/QrCodeService.java
@@ -1,7 +1,43 @@
 package com.qrgate.core.service;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class QrCodeService {
+
+    public String generateQrCodeBase64(String content, int width, int height)
+            throws WriterException, IOException {
+
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
+        hints.put(EncodeHintType.MARGIN, 2);
+
+        QRCodeWriter writer = new QRCodeWriter();
+        BitMatrix bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, width, height, hints);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+
+        byte[] qrBytes = outputStream.toByteArray();
+        return Base64.getEncoder().encodeToString(qrBytes);
+    }
+
+    public String generateQrCodeDataUri(String content, int width, int height)
+            throws WriterException, IOException {
+        String base64 = generateQrCodeBase64(content, width, height);
+        return "data:image/png;base64," + base64;
+    }
 }
